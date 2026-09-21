3524d04359c0c32b1d01becb6a19cbd7b680af5f
Bucksmon <74790878+Bucksmon@users.noreply.github.com>
2026-09-21 11:59:20 +0400

    feat: wire upload manager into server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,20 @@
 import { getEnv } from "./config/env.js";
 import { connectDatabase } from "./db/mongoose.js";
+import { createUploadManager } from "./services/uploadManager.js";
 import { createApp } from "./app.js";
 
 const env = getEnv();
 
 await connectDatabase(env.mongodbUri);
 
-const app = createApp({ frontendUrl: env.frontendUrl });
+const uploadManager = createUploadManager({
+  imgurClientId: env.imgurClientId
+});
+
+const app = createApp({
+  frontendUrl: env.frontendUrl,
+  uploadManager
+});
 
 app.listen(env.port, () => {
   console.log(`GTAW Image Manager API listening on port ${env.port}`);
