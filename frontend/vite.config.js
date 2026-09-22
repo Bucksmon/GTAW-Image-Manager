3abfe14587c5e525a6ff0357d267d4e8d28aa035
Bucksmon <74790878+Bucksmon@users.noreply.github.com>
2026-09-22 10:26:24 +0400

    refactor: update Pages base path

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/GTAW-Image-Manager/",
+  base: "/GTAW-Image-Manager-Web/",
   plugins: [react()]
 });
